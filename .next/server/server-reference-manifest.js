5787bbac24c2d0141aa6a36cb1d1d23c5785c3ff
Huy <146599764+Venomsnaker@users.noreply.github.com>
2024-01-31 21:41:44 +0700

    Configure Image Optimization

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"epXnSAT0kkKFC4wt0sJK0qXsG3Dtoq4zUtvSYs+KslU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"VOkwEjZGK/Iu55txx2vF0ruqNjjpEovCfc/Jsz7FhfU=\"\n}"
